dc1c6a1c0292a58803e5a506b23bf786ece540bb
Muhammad Mujtaba Tahir <97501928+Muji47@users.noreply.github.com>
2024-03-06 11:35:43 +0500

    Update Modal.js

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,9 +1,9 @@
 import { Modal } from "antd";
 const InformationMessage = () => {
-  let secondsToGo = 3;
+  let secondsToGo = 5;
   const modal = Modal.success({
-    title: "Resend request",
-    content: `Wait sending in 30 Sec`,
+    title: 'Resend Code',
+    content: `Wait! for 30 sec verification code sending to you.`,
   });
   const timer = setInterval(() => {
     secondsToGo -= 1;
