22cfba5a29ab1b3ce716ac7c2d74abc946ce8fa8
Muhammad Mujtaba Tahir <97501928+Muji47@users.noreply.github.com>
2024-03-07 15:16:50 +0500

    Update CompanyInfo.js

--- src/components/CompanyInfo.js
+++ src/components/CompanyInfo.js
@@ -1,84 +1,149 @@
 import { ToTopOutlined } from "@ant-design/icons";
-import { Checkbox, Row, Col, Select, Button, Upload, Form,Input } from "antd";
+import {
+  Checkbox,
+  Row,
+  Col,
+  Select,
+  Button,
+  Upload,
+  Form,
+  Input,
+  InputNumber,
+  Typography,
+  message,
+} from "antd";
 import React, { useState } from "react";
 import "../style.css";
 
-function CompanyInfo({onFinish}) {
+function CompanyInfo({ onFinish }) {
   const [checkedValue, setCheckedValue] = useState(null);
+  const [upload, setUpload] = useState(true);
 
   const onChange = (e) => {
     setCheckedValue(e.target.checked ? e.target.value : null);
   };
   const onFinishFailed = (errorInfo) => {
-    console.log('Failed:', errorInfo);
+    console.log("Failed:", errorInfo);
   };
   const adjustInput = {
     borderRadius: "10px",
   };
   const options = [
     {
-      value:"australia",
+      value: "australia",
       label: "Australia",
     },
     {
-      value:"india",
+      value: "india",
       label: "India",
     },
     {
-      value:"pakistan",
+      value: "pakistan",
       label: "Pakistan",
     },
     {
-      value:"iraq",
+      value: "iraq",
       label: "Iraq",
     },
   ];
-  
+  const checkImageResolution = (file) => {
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+      img.src = window.URL.createObjectURL(file);
+      img.onload = () => {
+        if (img.width <= 800 && img.height <= 600) {
+          resolve();
+          setUpload(true);
+        } else {
+          reject("Image resolution must be lower than or equal to 800x600.");
+          setUpload(false);
+        }
+      };
+    });
+  };
+
+  const handleBeforeUpload = async (file) => {
+    try {
+      await checkImageResolution(file);
+      return true;
+    } catch (error) {
+      message.error(error);
+      return false;
+    }
+  };
+
+  const handleKeyDown = (event) => {
+    if (!(event.key >= '0' && event.key <= '9') && event.key !== 'Backspace' && event.key !== 'Delete' && event.key !== 'ArrowLeft' && event.key !== 'ArrowRight') {
+      event.preventDefault();
+    }
+  };
   return (
     <div style={{ width: "78%", margin: "auto" }}>
       <Form onFinish={onFinish} onFinishFailed={onFinishFailed}>
-        <Row  justify={"space-between"} gutter={6}>
-          <Col xs={4} sm={6} md={8} lg={11}>
-            <div style={{display:"flex",justifyContent:"space-around",marginBottom:"20px"}}>
-            <Checkbox
-              value="Company"
-              checked={checkedValue === "Company"}
-              onChange={onChange}
-              size="large"
-              className="custom-checkbox"
+        <Row gutter={16}>
+          <Col xs={4} sm={6} md={8} lg={12}>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-around",
+                marginBottom: "20px",
+              }}
             >
-              Company
-            </Checkbox>
-            <Checkbox
-              value="Individual"
-              checked={checkedValue === "Individual"}
-              onChange={onChange}
-              size="large"
-              className="custom-checkbox"
-            >
-              Individual
-            </Checkbox>
+              <Checkbox
+                value="Company"
+                checked={checkedValue === "Company"}
+                onChange={onChange}
+                size="large"
+                className="custom-checkbox"
+              >
+                Company
+              </Checkbox>
+              <Checkbox
+                value="Individual"
+                checked={checkedValue === "Individual"}
+                onChange={onChange}
+                size="large"
+                className="custom-checkbox"
+              >
+                Individual
+              </Checkbox>
             </div>
             <Form.Item
               name="Company Name"
-              rules={[{ required: true, message: "Please enter company name!" }]}
+              rules={[
+                { required: true, message: "Please enter company name!" },
+              ]}
             >
-              <Input allowClear style={adjustInput} placeholder="Company Name" size='large' className="inputData"/>
+              <Input
+                allowClear
+                style={adjustInput}
+                placeholder="Company Name"
+                size="large"
+                className="inputData"
+              />
             </Form.Item>
-            <Form.Item
-              name="Company Person Name"
-            >
-              <Input allowClear style={adjustInput} placeholder="Company Person Name" size='large' className="inputData"/>
+            <Form.Item name="Company Person Name">
+              <Input
+                allowClear
+                style={adjustInput}
+                placeholder="Company Person Name"
+                size="large"
+                className="inputData"
+              />
             </Form.Item>
           </Col>
           <Col
             className="uploadIcon"
-            style={{ textAlign: 'right' }}
           >
-            <Upload maxCount={1} listType="picture">
+            <Upload
+              maxCount={1}
+              listType="picture"
+              beforeUpload={handleBeforeUpload}
+              showUploadList={upload ? true : false}
+            >
               <ToTopOutlined
                 style={{
-                  fontSize: "6rem",
+                  fontSize: "5rem",
                   color: "white",
                   backgroundColor: "#8920aa",
                   borderRadius: "10px",
@@ -88,79 +153,129 @@ function CompanyInfo({onFinish}) {
                 }}
               />
               Upload Company Logo
+              <Typography.Text style={{ display: "block" }}>
+                800x600px
+              </Typography.Text>
             </Upload>
           </Col>
         </Row>
-        <Row justify={"space-between"} gutter={6}>
-          <Col xs={4} sm={6} md={8} lg={11}>
+        <Row gutter={16}>
+          <Col xs={12} sm={12} md={12} lg={12}>
             <Form.Item
               name="Email"
-              rules={[{ required: true,type:"email", message: "Please enter email!" }]}
+              rules={[
+                {
+                  required: true,
+                  type: "email",
+                  message: "Please enter email!",
+                },
+              ]}
             >
-              <Input allowClear style={adjustInput} placeholder="Email" size='large' className="inputData"/>
+              <Input
+                allowClear
+                style={adjustInput}
+                placeholder="Email"
+                size="large"
+                className="inputData"
+              />
             </Form.Item>
           </Col>
-          <Col xs={4} sm={6} md={8} lg={11}>
+          <Col xs={12} sm={12} md={12} lg={12}>
             <Form.Item
               name="Phone"
             >
-              <Input allowClear type="number" style={adjustInput} placeholder="Phone" size='large' className="inputData"/>
+              <InputNumber
+                addonBefore="+61"
+                maxLength={10}
+                style={{
+                  width: "100%",
+                  
+                }}
+                placeholder="Phone"
+                size="large"
+                controls={false}
+                className="inputData"
+                onKeyDown={handleKeyDown}
+              />
+            </Form.Item>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col span={24}>
+            <Form.Item name="Address 1">
+              <Input
+                allowClear
+                style={adjustInput}
+                placeholder="Address 1"
+                size="large"
+                className="inputData"
+              />
             </Form.Item>
           </Col>
         </Row>
         <Row>
           <Col span={24}>
-            <Form.Item
-              name="Address 1"
-            >
-              <Input allowClear style={adjustInput} placeholder="Address 1" size='large' className="inputData"/>
-            </Form.Item>
-          </Col>
-        </Row>
-        <Row>
-          <Col span={24}>
-            <Form.Item
-              name="Address 2"
-            >
-              <Input allowClear style={adjustInput} placeholder="Address 2" size='large' className="inputData"/>
+            <Form.Item name="Address 2">
+              <Input
+                allowClear
+                style={adjustInput}
+                placeholder="Address 2"
+                size="large"
+                className="inputData"
+              />
             </Form.Item>
           </Col>
         </Row>
         <Row gutter={12} justify="space-between">
-          <Col xs={{ span: 5 }} lg={{ span: 7 }}>
+          <Col xs={{ span: 5 }} lg={{ span: 8 }}>
             <Form.Item
               name="City"
               rules={[{ required: true, message: "Please enter city!" }]}
             >
-              <Input allowClear style={adjustInput} placeholder="City" size='large' className="inputData"/>
+              <Input
+                allowClear
+                style={adjustInput}
+                placeholder="City"
+                size="large"
+                className="inputData"
+              />
             </Form.Item>
           </Col>
-          <Col xs={{ span: 5 }} lg={{ span: 7, offset: 1 }}>
+          <Col xs={{ span: 5 }} lg={{ span: 8 }}>
             <Form.Item
               name="Zip Code"
               rules={[{ required: true, message: "Please enter zip code!" }]}
             >
-               <Input allowClear style={adjustInput} placeholder="Zip code" size='large' className="inputData"/>
+              <Input
+                allowClear
+                style={adjustInput}
+                placeholder="Zip code"
+                size="large"
+                className="inputData"
+              />
             </Form.Item>
           </Col>
-          <Col xs={{ span: 5 }} lg={{ span: 7, offset: 1 }}>
+          <Col xs={{ span: 5 }} lg={{ span: 8 }}>
             <Form.Item>
-            <Select
-              className="inputData"
-              size={"large"}
-              defaultValue="Australia"
-              options={options}
-              popupClassName="custom-dropdown"
-              placement="bottomLeft"
-              rules={[{ required: true, message: "Please enter select country!" }]}
-            />
+              <Select
+                className="inputData"
+                size={"large"}
+                defaultValue="Australia"
+                options={options}
+                popupClassName="custom-dropdown"
+                placement="bottomLeft"
+                rules={[
+                  { required: true, message: "Please enter select country!" },
+                ]}
+              />
             </Form.Item>
           </Col>
         </Row>
-        <Form.Item >
-        <Button className="page-btn infoBtn"  htmlType="submit">
-          Save & Next
-        </Button>
+        <Form.Item>
+          <Button className="page-btn infoBtn" htmlType="submit">
+            Save & Next
+          </Button>
         </Form.Item>
       </Form>
     </div>
